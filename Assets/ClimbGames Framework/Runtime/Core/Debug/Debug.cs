6b265a58fbfdc46d17e1b837ee3c219bdbfe5042
donguk <devdonguk@gmail.com>
2026-04-03 18:53:22 +0900

    Debug.cs hide callstack attr 추가

--- Assets/ClimbGames Framework/Runtime/Core/Debug/Debug.cs
+++ Assets/ClimbGames Framework/Runtime/Core/Debug/Debug.cs
@@ -1,27 +1,32 @@
 using System.Diagnostics;
+using UnityEngine;
 
 namespace ClimbGames
 {
     public static class Debug
     {
+        [HideInCallstack]
         [Conditional("UNITY_EDITOR"), Conditional("DEV_BUILD"), Conditional("QA_BUILD")]
         public static void Log(object message)
         {
             UnityEngine.Debug.Log(message);
         }
 
+        [HideInCallstack]
         [Conditional("UNITY_EDITOR"), Conditional("DEV_BUILD"), Conditional("QA_BUILD")]
         public static void Assert(bool condition, object message)
         {
             UnityEngine.Debug.Assert(condition, message);
         }
 
+        [HideInCallstack]
         [Conditional("UNITY_EDITOR"), Conditional("DEV_BUILD"), Conditional("QA_BUILD")]
         public static void LogWarning(object message)
         {
             UnityEngine.Debug.LogWarning(message);
         }
 
+        [HideInCallstack]
         [Conditional("UNITY_EDITOR"), Conditional("DEV_BUILD"), Conditional("QA_BUILD"), Conditional("LIVE_BUILD")]
         public static void LogError(object message)
         {
